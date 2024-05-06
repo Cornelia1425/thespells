8430187cd733c83ec0cae917e543addf72b2da74
yiranshu <shuyiran1425@hotmail.com>
2024-05-06 14:52:19 -0400

    checkpoint0506

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     <br/> <br/>
     <MusicHeader />
 
-    <BgImages />
+ {/*    <BgImages /> */}
 
     <SpellsControl />
 
--- src/components/SpellDetail.jsx
+++ src/components/SpellDetail.jsx
@@ -6,10 +6,12 @@ export default function SpellDetail ({spell}){
     return (
 
         <> 
+        <div className="spellDetail">
             <p id="spellName">{spell.attributes.name || "Not Found 🥲"}</p>
             <p id="spellHand">{spell.attributes.hand || "Not Found 🥲"}</p>
             <p id="spellIncantation">{spell.attributes.incantation || "Not Found 🥲"}</p>
             <img id="spellImg" src={spell.attributes.image || './src/assets/img/21_wandPlaceholder.png'} alt="Spell" />
+        </div>
         
         </>
     )
